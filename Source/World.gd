e2cae5f36d3542884cb302af17ff18df19fdee82
Norodix <peceli.bence@gmail.com>
2021-08-27 20:47:08 +0200

    Infinite spawners created, POC done

--- Source/World.gd
+++ Source/World.gd
@@ -4,28 +4,42 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+const SCREEN_HEIGHT = 480
 var LogSpawnerResource = preload("res://LogSpawner.tscn")
 var spawners = []
+var globalY = 0
 
 func createSpawner(y, velocity):
 	spawners.append(LogSpawnerResource.instance())
 	spawners[-1].yPos = y
 	spawners[-1].velocity = velocity
 	self.add_child(spawners[-1])
-	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	createSpawner(368, 100)
-	createSpawner(368-(32+16), -100)
-	createSpawner(368-(32+16)*2, 100)
-	createSpawner(368-(32+16)*3, -100)
-	createSpawner(368-(32+16)*4, 100)
-	createSpawner(368-(32+16)*5, -100)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#create logspawners around visible area
+	globalY = -self.get_canvas_transform().origin.y
+	
+	#get min Y coord spawner
+	var minY = SCREEN_HEIGHT
+	var maxY = -INF
+	var minSpawner
+	
+	for spawner in spawners:
+		if (spawner.yPos < minY):
+			minSpawner = spawner
+			minY=spawner.yPos
+		maxY = max(maxY, spawner.position.y)
+	
+	#if not outside top of frame, add a new one with opposing velocity above it
+	if (globalY < minY):
+		createSpawner(minY - 48, - minSpawner.velocity)
+	
+	#TODO delete spawners outside the frame
 	pass
